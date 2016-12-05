30a3044ff7444879c2c90993ef4fd455b05e061d
Mario Taferner <mario.taferner@gmail.com>
2016-12-05 19:01:39 +0100

    added eurovision song contest

--- app/reducers/bingo.js
+++ app/reducers/bingo.js
@@ -1,19 +1,20 @@
 import * as types from '../actions/actionTypes';
 
 const initialState = {
-  decksAll: ['Harry Potter', 'Meeting', 'Lord of the Rings'],
+  decksAll: ['Harry Potter', 'Meeting', 'Lord of the Rings', 'Eurovision Song Contest'],
   deckSelected: false,
-  terms: ['Hermine', 'Ron', 'Harry', 'Serious', 'Hagrid',
-    'Dumbledore', 'Voldemort', 'Petunia', 'Snape', 'Goyle',
-    'Fred', 'Ginny', 'George', 'Dudley', 'Fleur', 'Lupin',
-    'Dolores', 'Gandalf', 'Bilbo', 'Sam', 'Aragorn',
-    'Frodo', 'Legolas', 'Gollum', 'Arwen', 'Gimli',
-    'Elrond', 'Boromir', 'Nekromant', 'Galadriel'],
-  termsSelected: ['Hermine', 'Ron', 'Harry', 'Serious', 'Hagrid',
-    'Dumbledore', 'Voldemort', 'Petunia', 'Snape', 'Goyle',
-    'Fred', 'Ginny', 'George', 'Dudley', 'Fleur',
-    'Dolores', 'Gandalf', 'Bilbo', 'Sam', 'Aragorn',
-    'Frodo', 'Legolas', 'Gollum', 'Arwen', 'Gimli'],
+  terms: ['Im Vorjahr gab es einen ähnlichen Song', 'Schräge Vögel sind dabei', 'Irgendwer erwähnt \"Ein bisschen Frieden\"',
+  'Germany: 6 Points', 'Auftritt einer Boygroup', 'Falsche Brüste, falsche \nWimpern & falsche Haare',
+  'Kein außergewöhnlicher Sound \nund vorhersehbare Tanzschritte', 'Es gibt einen Schwiegersohnliebling',
+  'Outfits der Teilnehmer \nleuchten wie Diskokugeln', 'Nachbarschaftshilfe bei Ostblock-Staaten',
+  'Bei Make-up gilt: Klotzen statt Kleckern', '\"This is ... calling\"', 'Mindestens eine Landesmoderatorin verwendet Botox',
+  '\"It\'s Eurovision Tradition\"', 'Im letzten Jahr hat dieser Stil funktioniert', 'Ralph Siegel ist wieder dabei',
+  'Übertriebenes Flaggenwedeln \nund Verteilen von Handküsschen', 'Punkte-Mafia bei den kleineren Ländern',
+  '\"Thank you sooooooo \nmuch for the wonderful show!\"', 'Interpret gibt sich seinem \nLied voll und ganz hin',
+  'Geschichtsträchtiger Song', 'Background Sänger singen \nbesser als eigentliche Performer',
+  'Spar-Grand-Prix mit nur einer \nModeratorin und weniger Budget', 'Irgendjemand spielt an der Nebelmaschine',
+  'Kleider der Sängerinnen bedecken \nnur das Notwendigste'],
+  termsSelected: [],
   started: false
 };
 
